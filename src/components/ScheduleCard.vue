02d82d0caa52669eb21d967e801c416a3b018927
Cameron Collver <crcollver@gmail.com>
2020-11-12 19:02:13 -0800

    feat: card component for dashboard

---
+++ src/components/ScheduleCard.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-hover v-slot="{ hover }">
+    <v-card :elevation="hover ? 3 : 0" outlined rounded="rounded-lg">
+      <v-card-title class="pb-0">
+        Title of schedule to get people's attention.
+      </v-card-title>
+      <v-card-text class="pb-0">
+        <!-- Max of 3 or 4 most likely -->
+        <v-container class="py-0">
+          <v-row>
+            <v-col cols="12" sm="8" class="px-0">
+              <v-chip class="mr-2" color="success" outlined small
+                >MATH485</v-chip
+              >
+              <v-chip class="mr-2" color="success" outlined small
+                >MATH374</v-chip
+              >
+              <v-chip class="mr-2" color="success" outlined small
+                >MATH441</v-chip
+              >
+            </v-col>
+            <v-col cols="12" sm="4" class="px-0">
+              <div class="d-flex justify-end">
+                Nov. 12th 2020 | 6:30PM
+              </div>
+            </v-col>
+          </v-row>
+        </v-container>
+        <v-divider></v-divider>
+        <v-card-actions class="pa-0">
+          <v-list-item two-line class="px-0">
+            <v-icon large left>mdi-account-circle</v-icon>
+            <v-list-item-content>
+              <v-list-item-title>
+                username
+              </v-list-item-title>
+              <v-list-item-subtitle>
+                email@cougars.csusm.edu
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-spacer></v-spacer>
+          <v-icon>mdi-star-plus-outline</v-icon>
+        </v-card-actions>
+      </v-card-text>
+    </v-card>
+  </v-hover>
+</template>
+
+<script>
+export default {
+  name: "ScheduleCard",
+};
+</script>
+
+<style scoped>
+.v-card {
+  transition: 0.2s ease;
+}
+.pointer {
+  cursor: pointer;
+}
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,9 +1,26 @@
 <template>
-  <div></div>
+  <v-container width="1100">
+    <v-row>
+      <v-col cols="12" md="8">
+        <ScheduleCard />
+      </v-col>
+      <v-col cols="12" md="4">
+        <v-card elevation="0" outlined rounded="rounded-lg" height="600">
+          <v-card-title>
+            Advanced Search
+          </v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import ScheduleCard from "../components/ScheduleCard";
 export default {
   name: "Dashboard",
+  components: {
+    ScheduleCard
+  }
 };
 </script>
