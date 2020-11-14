8baf064facf4e454c8261baf0c43928e8e052e99
Cameron Collver <crcollver@gmail.com>
2020-11-13 22:01:35 -0800

    fix(API): replaced axios const instance with a function that returns instance instead

--- src/API/index.js
+++ src/API/index.js
@@ -2,28 +2,29 @@ import axios from "axios";
 import store from "../store";
 
 // '/' is included at end of base URL, so write API calls accordingly!
-const token = store.state.token;
-const api = axios.create({
-  baseURL: "http://localhost:3000/",
-  headers: {
-    Authorization: `Bearer ${token}`,
-  },
-});
+const api = function() {
+  return axios.create({
+    baseURL: "http://localhost:3000/",
+    headers: {
+      Authorization: `Bearer ${store.state.token}`,
+    },
+  });
+};
 
 function register(data) {
-  return api.post("register", data);
+  return api().post("register", data);
 }
 
 function login(data) {
-  return api.post("login", data);
+  return api().post("login", data);
 }
 
 function postSchedule(scheduleObj) {
-  return api.post("schedules", scheduleObj);
+  return api().post("schedules", scheduleObj);
 }
 
 function getScheduleById(scheduleID) {
-  return api.get(`schedules/${scheduleID}`);
+  return api().get(`schedules/${scheduleID}`);
 }
 
 // Other API calls go here
